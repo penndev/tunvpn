5697e8cf4b5e6b370f239512c25d845f5c0faafb
penn <pennilessfor@gmail.com>
2023-06-24 22:55:28 +0800

    调整代理权限

--- app/src/main/java/com/github/penndev/MainActivity.kt
+++ app/src/main/java/com/github/penndev/MainActivity.kt
@@ -2,11 +2,11 @@ package com.github.penndev
 
 import android.content.Intent
 import android.net.VpnService
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
