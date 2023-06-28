7e45d3fe938c56bf7ae9a0acf857e6fefef9675c
penn <pennilessfor@gmail.com>
2023-06-28 23:09:15 +0800

    调整自定义通知栏

--- app/src/main/java/com/github/penndev/TunService.kt
+++ app/src/main/java/com/github/penndev/TunService.kt
@@ -1,6 +1,5 @@
 package com.github.penndev
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,7 +8,9 @@ import android.net.VpnService
 import android.os.Build
 import android.os.ParcelFileDescriptor
 import android.util.Log
+import android.widget.RemoteViews
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -29,6 +30,16 @@ class TunService : VpnService() {
     private var servicePort:Int = 0
     private var servicePassword:String = ""
 
+    //通知
+    private val NOTIFICATION_ID = 1
+    private val CHANNEL_ID = "tunVpnNotify"
+    private val CHANNEL_NAME = "tunVpnNotify"
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+    private lateinit var notificationView: RemoteViews
+
+
     private fun setupCommand(intent: Intent?) {
         if(intent == null){
             throw Exception("传参错误")
@@ -46,106 +57,62 @@ class TunService : VpnService() {
         servicePassword = intent.getStringExtra("servicePassword")!!
     }
 
+    private fun setupVpnServe(){
+        setupNotifyForeground() // 初始化启动通知
+        // 获取远程认证。并获取本地的IP配置dns等等。
+
+
+        job = GlobalScope.launch {
+            var i = 0
+            while (true){
+                delay(5000)
+                Log.i("penndev", "debug print here123->$i")
+                updateNotification("debug print here123->$i")
+                i ++
+            }
+        }
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // 处理重复启动，关闭等操作。
         try {
             setupCommand(intent)
         }catch (e: Exception){ //处理抛出异常问题
             Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
             return START_NOT_STICKY
         }
-        // 转换为前端服务
-        setupForeground()
-        // 启动/dev/tun 设备
-        //setupBuilder()
-
-        job = GlobalScope.launch {
-            while (true){
-                delay(5000)
-                Log.i("penndev","debug print here123->")
-
-            }
-        }
-
         return START_NOT_STICKY
     }
 
+    private fun setupNotifyForeground() {
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
-
-    //
-    //// 创建本地tun设备
-    //val tunFd = localTunnel?.fileDescriptor
-    //val tunRead = FileInputStream(tunFd)
-    //val tunWrite = FileOutputStream(tunFd)
-    //
-    //// 创建 udp socket
-    //val sock = DatagramChannel.open()
-    //protect(sock.socket())
-    //val client = InetSocketAddress(serviceIp,servicePort)
-    //sock.connect(client)
-    //sock.configureBlocking(false)
-    //
-    //var readCount = 0
-    //val readBuf = ByteBuffer.allocate(32767)
-    //while (isActive) try{
-    //    readCount ++
-    //
-    //    val tunReadLen = tunRead.read(readBuf.array())
-    //    if(tunReadLen > 0){
-    //        readCount = 0
-    //        readBuf.limit(tunReadLen)
-    //        sock.write(readBuf)
-    //        readBuf.clear()
-    //        Log.i("penndev", "sock read from tun : $tunReadLen")
-    //    }
-    //
-    //    val sockReadLen = sock.read(readBuf)
-    //    if (sockReadLen > 0){
-    //        readCount = 0
-    //        readBuf.limit(tunReadLen)
-    //        tunWrite.write(readBuf.array(),0,sockReadLen)
-    //        readBuf.clear()
-    //        Log.i("penndev", "tun read from sock : $sockReadLen")
-    //    }
-    //
-    //    if (readCount > 5){
-    //        Thread.sleep(100);
-    //    }
-    //}catch (e: Exception){
-    //    Log.i("penndev", "Job Err: $e")
-    //    return@launch
-    //}
-
-
-    //private fun setupBuilder(): FileDescriptor {
-    //    val builder = Builder()
-    //    localTunnel = builder
-    //        .setMtu(1400)
-    //        .addAddress("10.0.0.200", 32)
-    //        .addRoute("0.0.0.0", 0)
-    //        .addDnsServer("8.8.8.8")
-    //        .establish()
-    //    if(localTunnel == null){
-    //        throw Exception("启动隧道失败")
-    //    }
-    //    return localTunnel!!.fileDescriptor
-    //}
-
-    private fun setupForeground(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = getText(R.string.app_name).toString()
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_DEFAULT))
-            var intent = Intent(this, MainActivity::class.java)
-            var pendingIntent =  PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
-            val notice = Notification.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentIntent(pendingIntent)
-                .setContentTitle("服务器标题")
-                .setContentText("服务器前台启动中")
-                .build()
-            startForeground(1, notice)
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
         }
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        notificationView = RemoteViews(packageName, R.layout.notification_layout)
+        notificationView.setTextViewText(R.id.notificationTitle, "TunVPN")
+        notificationView.setTextViewText(R.id.notificationContent, "正在连接服务器")
+
+        notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setCustomContentView(notificationView)
+            .setContentIntent(pendingIntent)
+            .setOngoing(true)
+            .setOnlyAlertOnce(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun updateNotification(contentText: String) {
+        notificationView.setTextViewText(R.id.notificationContent, contentText)
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
     }
 
     override fun onDestroy() {
