a5bfecc089215bd64a8708662ae4db27d6433554
penn <pennilessfor@gmail.com>
2023-07-05 14:54:41 +0800

    add the remote udp connect

--- app/src/main/java/com/github/penndev/TunService.kt
+++ app/src/main/java/com/github/penndev/TunService.kt
@@ -13,44 +13,61 @@ import android.widget.RemoteViews
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.*
+import java.io.FileDescriptor
 import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.DatagramChannel
 
 
 class TunService : VpnService() {
-    // status 开启状态
-    private var status: Boolean = false
+
     // tun 设备
-    private var localTunnel: ParcelFileDescriptor? = null
+    private var tun: ParcelFileDescriptor? = null
+    private lateinit var tunFd: FileDescriptor
+
     // 工作进程
     private var job: Job? = null
 
+    // status 开启状态
+    private var status: Boolean = false
+
     //远程服务器认证
-    private var serviceIp:String = ""
-    private var servicePort:Int = 0
-    private var servicePassword:String = ""
+    private lateinit var serviceSock: DatagramChannel
+    private var serviceIp: String = ""
+    private var servicePort: Int = 0
+    private var servicePassword: String = ""
 
     //通知
-    private val NOTIFICATION_ID = 1
-    private val CHANNEL_ID = "tunVpnNotify"
-    private val CHANNEL_NAME = "tunVpnNotify"
-
+    private val notifyID = 1
+    private val notifyChannelID = "tunVpnNotify"
+    private val notifyChannelName = "tunVpnNotify"
     private lateinit var notificationManager: NotificationManager
     private lateinit var notificationBuilder: NotificationCompat.Builder
     private lateinit var notificationView: RemoteViews
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // 处理重复启动，关闭等操作。
+        try {
+            setupCommand(intent)
+            setupVpnServe()
+        } catch (e: Exception) { //处理抛出异常问题
+            Log.e("penndev", "服务引起异常", e)
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+        return START_NOT_STICKY
+    }
 
     private fun setupCommand(intent: Intent?) {
-        if(intent == null){
+        if (intent == null) {
             throw Exception("传参错误")
         }
-        if(intent.getBooleanExtra("close",false)){
+        if (intent.getBooleanExtra("close", false)) {
             onDestroy()
             throw Exception("关闭成功")
         }
-        if (status){
+        if (status) {
             throw Exception("正在运行中")
         }
         status = true
@@ -59,64 +76,64 @@ class TunService : VpnService() {
         servicePassword = intent.getStringExtra("servicePassword")!!
     }
 
-    private fun setupVpnServe(){
-        setupNotifyForeground() // 初始化启动通知
+    private fun setService() {
+        serviceSock = DatagramChannel.open()
+        Log.i("penndev",protect(serviceSock.socket()).toString())
+        serviceSock.connect(InetSocketAddress(serviceIp, servicePort))
 
-
-        job = GlobalScope.launch {
-            // 避免线程阻塞
-            val sock = DatagramChannel.open()
-            protect(sock.socket())
-            val client = InetSocketAddress(serviceIp,servicePort)
-            sock.connect(client)
-
-            // 获取远程认证。并获取本地的IP配置dns等等。
-            localTunnel = Builder()
-                .addRoute("0.0.0.0", 0)
-                .addAddress("10.0.0.200", 32)
-                .establish()
-            if(localTunnel == null){
-                throw Exception("启动隧道失败")
-            }
-
-            val tunFd = localTunnel?.fileDescriptor
-            val tunRead = FileInputStream(tunFd)
-
-            //主要内容
-            val readBuf = ByteBuffer.allocate(32767)
-            while (isActive) try{
-                readBuf.clear()
-                val readBufLen = withContext(Dispatchers.IO) {
-                    tunRead.read(readBuf.array())
-                }
-                if(readBufLen > 0){
-                    readBuf.limit(readBufLen)
-                    sock.write(readBuf)
-                }
-            }catch (e: Exception){
-                Log.i("penndev", "Job Err: $e")
-                return@launch
-            }
-        }
-
-        //
-        //job = CoroutineScope(Dispatchers.Default).launch {
-        //    val readBuf = ByteBuffer.allocate(32767)
-        //    while (true){
-
-        //    }
-        //}
+        tun = Builder()
+            .setMtu(1400)
+            .addDnsServer("8.8.8.8")
+            .addRoute("0.0.0.0", 0)
+            .addAddress("10.0.0.2", 32)
+            .establish() ?: throw Exception("启动隧道失败")
+        tunFd = tun?.fileDescriptor!!
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // 处理重复启动，关闭等操作。
-        try {
-            setupCommand(intent)
-            setupVpnServe()
-        }catch (e: Exception){ //处理抛出异常问题
-            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+    private fun setupVpnServe() {
+        setupNotifyForeground() // 初始化启动通知
+
+        job = GlobalScope.launch {
+            setService()
+            updateNotification("发送通知完成")
+
+            val readSock = launch {
+                val packet = ByteBuffer.allocate(32767)
+                val tunWrite = FileOutputStream(tunFd)
+                serviceSock.configureBlocking(false)
+                while (true){
+                    val readLen = serviceSock.read(packet)
+                    if (readLen > 0){
+                        if (packet[0].toInt() != 0) {
+                            tunWrite.write(packet.array(),0,readLen)
+                        }
+                        packet.clear()
+                    }else {
+                        delay(100)
+                    }
+                }
+            }
+
+            var readTun = launch {
+                val packet = ByteBuffer.allocate(32767)
+                val tunReader = FileInputStream(tunFd)
+                while (true){
+                    val readLen = tunReader.read(packet.array())
+                    if (readLen > 0){
+                        packet.limit(readLen)
+                        serviceSock.write(packet)
+                        packet.clear()
+                    }else {
+                        delay(100)
+                    }
+                }
+            }
+
+            readSock.join()
+            readTun.join()
         }
-        return START_NOT_STICKY
+
+
     }
 
     @SuppressLint("UnspecifiedImmutableFlag")
@@ -124,18 +141,20 @@ class TunService : VpnService() {
         notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            val channel = NotificationChannel(notifyChannelID, notifyChannelName, NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
         }
 
         val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        //val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
 
         notificationView = RemoteViews(packageName, R.layout.notification_layout)
         notificationView.setTextViewText(R.id.notificationTitle, "TunVPN")
         notificationView.setTextViewText(R.id.notificationContent, "正在连接服务器")
 
-        notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+        notificationBuilder = NotificationCompat.Builder(this, notifyChannelID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setCustomContentView(notificationView)
             .setContentIntent(pendingIntent)
@@ -143,24 +162,24 @@ class TunService : VpnService() {
             .setOnlyAlertOnce(true)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        startForeground(notifyID, notificationBuilder.build())
     }
 
     private fun updateNotification(contentText: String) {
         notificationView.setTextViewText(R.id.notificationContent, contentText)
-        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+        notificationManager.notify(notifyID, notificationBuilder.build())
     }
 
     override fun onDestroy() {
         job?.cancel()
-        localTunnel?.close()
+        tun?.close()
         status = false
         stopForeground(true)
         super.onDestroy()
     }
 
     override fun onRevoke() {
-        Toast.makeText(this,"TunVPN已停止",Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "TunVPN已停止", Toast.LENGTH_LONG).show()
         onDestroy()
     }
 }
